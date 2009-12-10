0aa1f5d650fef38864678ae22d884442fd632049
acox.roaming <acox.roaming@5c4f93ee-e37c-11de-b184-cf21a9d0b08a>
2009-12-10 17:57:09 +0000

    Add prolonged flood test to testMail script

--- trunk/testMail.py
+++ trunk/testMail.py
@@ -1,11 +1,14 @@
 #!/usr/local/bin/python
-import smtplib, time, threading, sys, random
+import smtplib, time, threading, sys, random, datetime
 from email.mime.text import MIMEText
 
 fromaddr = sys.argv[1].split(",")
 toaddr = sys.argv[2]
 mailsToSend = 20
 threadCount = 10
+sleepBetweenSends=0.1
+sustainedFloodForSeconds=60
+
 
 def createMessage(fromaddr, toaddr, subject, msgtxt):
     msg = MIMEText(msgtxt)
@@ -19,9 +22,18 @@ def sendMails(threadId):
     for i in xrange(mailsToSend):
         actualFrom = random.choice(fromaddr)
         server.sendmail(actualFrom, [toaddr], createMessage(actualFrom, toaddr, "This is from thread %s" % threadId, "Some header" ).as_string())
+        
     server.quit()
 
-threads = [threading.Thread(target=sendMails, args=(i,)) for i in range(threadCount)]
+def sendMailsFor(threadId):
+    start = datetime.datetime.today()
+    while ( datetime.datetime.today() - start ) < datetime.timedelta(seconds=sustainedFloodForSeconds):
+        sendMails(threadId)
+        if sleepBetweenSends:
+            time.sleep(sleepBetweenSends)
+    
+
+threads = [threading.Thread(target=sendMailsFor, args=(i,)) for i in range(threadCount)]
 
 for t in threads:
     t.start()
