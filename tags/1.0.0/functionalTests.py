33e1f6d0283882b231d530bcee345962c7a12c56
acox.roaming <acox.roaming@5c4f93ee-e37c-11de-b184-cf21a9d0b08a>
2010-01-12 12:21:26 +0000

    Tag for first release

---
+++ tags/1.0.0/functionalTests.py
@@ -0,0 +1,142 @@
+#!/usr/local/bin/python
+# Copyright (c) Andrew Cox 2009-10
+# See LICENCE for details
+"""
+Functional tests for the mailserver
+"""
+#Python BulitIns
+import smtplib, time, threading, sys, random, datetime, os, urllib
+from email.mime.text import MIMEText
+
+maildroplocation = "maildrop"
+fromAddr = "abi@ssgl.com"
+fromAddrs = ["abi1@ssgl.com", "abi2@ssgl.com" ,"abi3@ssgl.com"]
+toAddr = "ac@ssgl.com"
+msgBody = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum posuere diam vel venenatis.
+Mauris sed diam lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+Nunc ut lacus in nunc fringilla ullamcorper nec id dolor. Suspendisse ut nulla ut risus viverra fermentum eu sit amet neque.
+Nam tincidunt tempus tempus. Sed sollicitudin sapien eget justo lacinia tincidunt. Quisque ultrices euismod ornare.
+Ut at ante orci. Quisque congue porta odio sit amet interdum. Nulla auctor fringilla leo, et pharetra diam vestibulum vel.
+Pellentesque nec sagittis purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus justo nibh, egestas eget fermentum id, luctus a turpis."""
+msgSubject = "Test"
+sleepTime = 5.0
+
+def createMessage(fromaddr, toaddr, subject, msgtxt):
+    msg = MIMEText(msgtxt)
+    msg['Subject'] = subject
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    return msg
+
+def sendMail(fromaddr, toaddr, msg):
+    server = smtplib.SMTP('localhost', 8001)
+    server.sendmail(fromaddr, [toaddr], msg.as_string())
+    server.quit()
+
+def cleanUpMailDrop():
+    for fp in os.listdir(maildroplocation):
+        os.remove( os.path.join( maildroplocation, fp ) )
+               
+def checkFileExists(numberToCheckFor=1):
+    return len(os.listdir(maildroplocation)) == numberToCheckFor
+
+def forceClear():
+    urllib.urlopen("http://127.0.0.1:8080/forceclear")
+
+
+def test_delayInSend():
+    """Tests that there is a delay in sending the emails 
+    """
+    msg = createMessage(fromAddr, toAddr, msgSubject, msgBody)
+    cleanUpMailDrop()
+    sendMail(fromAddr, toAddr, msg)
+    if checkFileExists():
+        print "TEST CASE - BAD"
+    time.sleep(sleepTime)
+    if checkFileExists():
+        print "TEST CASE - OK"
+    else:
+        print "TEST CASE - BAD"
+    cleanUpMailDrop()
+
+def test_sendsEmailsWhenLessThanOverflow():
+    """Tests that if we send 19 emails in a second then it does not drop any
+    """
+    msg = createMessage(fromAddr, toAddr, msgSubject, msgBody)
+    cleanUpMailDrop()
+    for i in xrange(19):
+        sendMail(fromAddr, toAddr, msg)
+    time.sleep(sleepTime)
+    if checkFileExists(19):
+        print "TEST CASE - OK"
+    else:
+        print "TEST CASE - BAD"
+    cleanUpMailDrop()
+
+def test_sendsExcessEmailWhenMoreThanOverflow():
+    """Tests that if we send 21 emails in a second then it drops them and only sends a excess email
+    """
+    msg = createMessage(fromAddr, toAddr, msgSubject, msgBody)
+    cleanUpMailDrop()
+    for i in xrange(21):
+        sendMail(fromAddr, toAddr, msg)
+    time.sleep(sleepTime)
+    if checkFileExists(1):
+        print "TEST CASE - OK"
+    else:
+        print "TEST CASE - BAD"
+    cleanUpMailDrop()
+
+def test_sendsExcessEmailWhenMoreThanOverflowForProlongedPeriod():
+    """Tests that if we send lots of emails over a large period then it drops them and only sends a excess email
+    """
+    
+    msg = createMessage(fromAddr, toAddr, msgSubject, msgBody)
+    cleanUpMailDrop()
+    begin = datetime.datetime.today()
+    forceClear()
+    for i in xrange(9):
+        for i in xrange(21):
+            sendMail(fromAddr, toAddr, msg)
+        time.sleep(1)
+    time.sleep(sleepTime)
+    forceClear()
+    end = datetime.datetime.today()
+    #print end - begin    
+    if checkFileExists(1):
+        print "TEST CASE - OK"
+    else:
+        print "TEST CASE - BAD"
+    cleanUpMailDrop()
+
+def test_sendsExcessEmailWhenMoreThanOverflowWithMultipleFroms():
+    """Tests that if we send 21 emails from different uses in a second then it drops them and only sends a excess email
+    """
+    cleanUpMailDrop()
+    for fromAddr in fromAddrs:
+        msg = createMessage(fromAddr, toAddr, msgSubject, msgBody)
+        for i in xrange(21):
+            sendMail(fromAddr, toAddr, msg)
+    time.sleep(sleepTime)
+    if checkFileExists(1):
+        print "TEST CASE - OK"
+    else:
+        print "TEST CASE - BAD"
+    cleanUpMailDrop()
+
+
+
+
+def main():
+    funs = [globals()[f] for f in globals().keys() if f.startswith("test_")]
+    for fun in funs:
+        time.sleep(sleepTime)
+        print "*" *10 , fun.__name__ , "*" * 10
+        print fun.__doc__
+        fun()   
+    
+
+if __name__ == "__main__":
+    main()
+
---
+++ tags/1.0.0/mailThrottler.py
@@ -0,0 +1,66 @@
+#!/usr/local/bin/python
+# Copyright (c) Andrew Cox 2009-10
+# See LICENCE for details
+"""
+The main entry point for the email throttler
+"""
+#Internal Modules
+import mailServer, webServer, core, logging
+from core import _config
+#Python BulitIns
+import sys
+#External Modules
+from twisted.internet import reactor
+
+class MTDaemon(object):
+    
+    defaultValues = { "mailInterface": "", "webInterface" : "", "mailPort" : "8001", "webPort" : "8080" }
+    _config.importDefaults("MTDaemon", defaultValues)
+
+    def __init__(self, configPath = ""):
+        core.loggerSetup()
+        self.logger = logging.getLogger("MTDaemon")
+        self.loadConfig(configPath)
+        
+        self.counter = core.QueueMessageCounter()
+        self.mailDelivery = mailServer.LocalDelivery(self.counter)
+        self.mailFactory = mailServer.SMTPFactory(self.mailDelivery)
+        self.adminSite = webServer.createSite(self.mailDelivery)        
+        self.initaliseReactor()
+
+    def loadConfig(self, filepath):
+        """Loads a configuration file into the config object
+        - `filepath`: filepath to the config file (in INI format)
+        """
+        if filepath:
+            self.logger.debug("Loading Config @ %s", filepath)
+            core.loadConfig(filepath)
+    
+    def initaliseReactor(self):
+        """initalises the reactor with the mail and web servers
+        """
+        self.logger.debug("initalise reactor")
+        mailPort = _config.getint("MTDaemon", "mailPort")
+        mailInterface = _config.get("MTDaemon", "mailInterface")
+        reactor.listenTCP(mailPort, self.mailFactory ,interface=mailInterface)
+        
+        webPort = _config.getint("MTDaemon", "webPort")
+        webInterface = _config.get("MTDaemon", "webInterface")
+        reactor.listenTCP(webPort, self.adminSite,interface=webInterface)
+
+    def start(self):
+        self.logger.info("Starting Reactor")
+        reactor.run()
+
+    def stop(self):
+        self.logger.info("Stoping Reactor")
+        reactor.stop()
+        
+
+
+if __name__ == '__main__':
+    filepath = ""
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+    daemon = MTDaemon(filepath)
+    daemon.start()
